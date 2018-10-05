17633ef2c7a7291be2d8240fa0a1d43d6ee1e45b
Patrick Dillon <pdd@redhat.com>
2018-10-05 16:27:20 -0400

    Add ace-window. Switch tabs to spaces. Remove escape characters from shell.

--- init.el
+++ init.el
@@ -2,7 +2,10 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(setq-default indent-tabs-mode nil)
 
+(global-set-key (kbd "M-o") 'ace-window)
+ 
 ;; save all tempfiles in $TMPDIR/emacs$UID/                                                        
 (defconst emacs-tmp-dir (expand-file-name (format "emacs%d" (user-uid)) temporary-file-directory))
 (setq backup-directory-alist
@@ -15,8 +18,8 @@
 ;; (global-linum-mode 1)
 
 ;; startup dashboard
-(require 'dashboard)
-(dashboard-setup-startup-hook)
+;;(require 'dashboard)
+;;(dashboard-setup-startup-hook)
 
 ;;(set-frame-font "Inconsolata-14")
 (set-frame-font "DejaVu Sans Mono-14")
@@ -66,7 +69,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (dashboard xterm-color doom-themes material-theme kaolin-themes yaml-mode tablist powerline magit leuven-theme let-alist go-projectile git ggtags geben-helm-projectile elfeed-org angular-mode))))
+    (ace-window markdown-preview-eww markdown-preview-mode dashboard xterm-color doom-themes material-theme kaolin-themes yaml-mode tablist powerline magit leuven-theme let-alist go-projectile git ggtags geben-helm-projectile elfeed-org angular-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -81,3 +84,47 @@
 (load-theme 'material t)
 ;;(load-theme 'doom-nord-light t)
 ;;(load-theme 'doom-opera-light t)
+
+
+;; These functions remove ANSI escape characters from emacs shell
+;; for example, the docker run command tries to rename the window
+;; which results in a ^[] character in the output
+(defun preamble-regexp-alternatives (regexps)
+  "Return the alternation of a list of regexps."
+  (mapconcat (lambda (regexp)
+               (concat "\\(?:" regexp "\\)"))
+             regexps "\\|"))
+
+(defvar non-sgr-control-sequence-regexp nil
+  "Regexp that matches non-SGR control sequences.")
+
+(setq non-sgr-control-sequence-regexp
+      (preamble-regexp-alternatives
+       '(;; icon name escape sequences
+         "\033\\][0-2];.*?\007"
+         ;; non-SGR CSI escape sequences
+         "\033\\[\\??[0-9;]*[^0-9;m]"
+         ;; noop
+         "\012\033\\[2K\033\\[1F"
+         )))
+
+(defun filter-non-sgr-control-sequences-in-region (begin end)
+  (save-excursion
+    (goto-char begin)
+    (while (re-search-forward
+            non-sgr-control-sequence-regexp end t)
+      (replace-match ""))))
+
+(defun filter-non-sgr-control-sequences-in-output (ignored)
+  (let ((start-marker
+         (or comint-last-output-start
+             (point-min-marker)))
+        (end-marker
+         (process-mark
+          (get-buffer-process (current-buffer)))))
+    (filter-non-sgr-control-sequences-in-region
+     start-marker
+     end-marker)))
+
+(add-hook 'comint-output-filter-functions
+          'filter-non-sgr-control-sequences-in-output)
